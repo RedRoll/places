5abc53670ab9126f289cbd79c29a6f6739c18e7d
Volodymyr Benzyk <redrollofficial@gmail.com>
2025-02-06 12:15:24 +0100

    responsivity test 2

--- src/compnents/places/Places.jsx
+++ src/compnents/places/Places.jsx
@@ -13,7 +13,7 @@ const Places = ({ }) => {
     const { availablePlaces, addedPlaces} = useContext(PlaceContext)
 
     return (
-        <div className="flex flex-col gap-y-8">
+        <div className="flex flex-col gap-y-8 ">
             <PlacesContent title='My wish places.'  modalAction={setModalIsOpen} places={addedPlaces} />
             <PlacesContent title='Available places.' click={true} places={availablePlaces} /> {/*click щоб задіялась функція для альтернативного сценарію(додавання елементів до масиву)*/}
             <DialogAllert  modalState={modalIsopen} modalAction={setModalIsOpen} />
